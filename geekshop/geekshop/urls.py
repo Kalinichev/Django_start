a5e09a8aea65b689f1adb89e97ff2a51f6fb347c
GL <newkp9@gmail.com>
2022-05-10 20:50:13 +0300

    base.html, and some minor edits

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('products/', mainapp.products, name='products'),
     path('contact/', mainapp.contact, name='contact'),
     path('', mainapp.main, name='main'),
+    path('tmp_url', mainapp.tmp_url, name='tmp_url'),
 ]
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 
+content = {}
+
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', content)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    return render(request, 'mainapp/products.html', content)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    return render(request, 'mainapp/contact.html', content)
+
+
+def tmp_url(request):
+    return render(request, 'mainapp/base.html')
