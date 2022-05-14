8fc32d2ff761532f2d60c4f3f97c90b7ef54dba8
GL <newkp9@gmail.com>
2022-05-14 16:50:04 +0300

    new_index.html ready + inc_main_menu.html

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,28 +1,42 @@
 from django.shortcuts import render
 
 content = {
-    'links_main_menu': [
+    'main_menu': [
         {'href': 'main', 'name': 'домой'},
         {'href': 'products', 'name': 'продукты'},
         {'href': 'contact', 'name': 'контакты'},
     ]
-    # 'title': title,
-    # 'links_menu': links_menu,
-    # 'same_products': same_products
+
 }
 
 
 def main(request):
+    # own_content = {
+    #     'title': title,
+    # }
+    # content.update(own_content)
     return render(request, 'mainapp/index.html', content)
 
 
 def products(request):
+    # own_content = {
+    #     'title': title,
+    #     'links_menu': links_menu,
+    #     'same_products': same_products,
+    # }
     return render(request, 'mainapp/products.html', content)
 
 
 def contact(request):
+    # own_content = {
+    #     'title': title,
+    # }
     return render(request, 'mainapp/contact.html', content)
 
 
 def tmp_url(request):
+    own_content = {
+        'title': "магазин",
+    }
+    content.update(own_content)
     return render(request, 'mainapp/new_index.html', content)
