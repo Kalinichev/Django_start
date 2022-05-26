63876ad3cfaaad8c6a0a30a601d5160ede688b13
GL <newkp9@gmail.com>
2022-05-26 20:52:54 +0300

    admin / minor

--- geekshop/mainapp/admin.py
+++ geekshop/mainapp/admin.py
@@ -3,4 +3,9 @@ from .models import Product, ProductCategory
 
 admin.site.register(ProductCategory)
 admin.site.register(Product)
+
+
+def tmpfunc():
+    pass
+
 # Register your models here.
--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -22,7 +22,7 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(verbose_name='Количество товаров на складе', default=0)
 
     def __str__(self):
-        return f"{self.name} {self.category.name}"
+        return f"{self.name} ({self.category.name})"
 
     class Meta:
         db_table = 'product'
