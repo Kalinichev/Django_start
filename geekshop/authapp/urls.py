b3f8deabb271596e342a624f6625897e5682b871
GL <newkp9@gmail.com>
2022-06-08 21:38:15 +0300

    fill_db works

--- geekshop/authapp/urls.py
+++ geekshop/authapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from views import login, logout
+from .views import login, logout
 
 app_name = 'authapp'
 
--- geekshop/mainapp/management/commands/fill_db.py
+++ geekshop/mainapp/management/commands/fill_db.py
@@ -1,20 +1,20 @@
 from django.core.management.base import BaseCommand
 from mainapp.models import Product, ProductCategory
-from django.contrib.auth.models import User
+from authapp.models import ShopUser
 
 import json, os
 
 JSON_PATH = 'mainapp/json'
 
+
 def load_from_json(file_name):
-    with open(os.path.join(JSON_PATH, file_name + '.json'), 'r') as infile:
+    with open(os.path.join(JSON_PATH, file_name + '.json'), 'r', encoding='utf-8') as infile:
         return json.load(infile)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         categories = load_from_json('categories')
-
         ProductCategory.objects.all().delete()
         for category in categories:
             new_category = ProductCategory(**category)
@@ -30,6 +30,4 @@ class Command(BaseCommand):
             new_product = Product(**product)
             new_product.save()
 
-            super_user = User.objects.create_superuser('boss', 'boss@supershop.local', 'ZaQ1@wSx', age=34)
-
-
+        super_user = ShopUser.objects.create_superuser('boss', 'boss@supershop.local', 'ZaQ1@wSx', age=34)
