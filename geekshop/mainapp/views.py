c881fc1a5866ac7cad7656fa3c64a13efff84e7c
GL <newkp9@gmail.com>
2022-06-18 19:04:17 +0300

    products

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product, ProductCategory
+from django.shortcuts import get_object_or_404
 
 content = {
     'main_menu': [
@@ -24,23 +25,47 @@ def main(request):
 
 
 def products(request, pk=None):
-    links_menu = [
-        {'href': 'products_all', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-    ]
+    print(pk)
+
+    title = 'продукты'
+    # links_menu = [
+    #     {'href': 'products_all', 'name': 'все'},
+    #     {'href': 'products_home', 'name': 'дом'},
+    #     {'href': 'products_office', 'name': 'офис'},
+    #     {'href': 'products_modern', 'name': 'модерн'},
+    #     {'href': 'products_classic', 'name': 'классика'},
+    # ]
+
+    links_menu = ProductCategory.objects.all()
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk)
+
+        own_content = {
+            'title': title,
+            'links_menu': links_menu,
+            'products': products,
+            'category': category,
+        }
+
+        content.update(own_content)
+        return render(request, 'mainapp/products.html', content)
+
+    same_products = Product.objets.all()[3:5]
 
     own_content = {
-        'title': 'продукты',
+        'title': title,
         'links_menu': links_menu,
-        # 'same_products': same_products,
+        'same_products': same_products,
     }
 
     content.update(own_content)
     return render(request, 'mainapp/products.html', content)
-
 
 def contact(request):
     own_content = {
@@ -48,4 +73,3 @@ def contact(request):
     }
     content.update(own_content)
     return render(request, 'mainapp/contact.html', content)
-
